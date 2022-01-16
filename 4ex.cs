20ccdfadfa0e376222306817395d066fcb0b9491
Ovszer <97823660+Ovszer@users.noreply.github.com>
2022-01-16 12:06:54 +0300

    Add files via upload

---
+++ 4ex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырёхзначное число");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int N1 = (N / 1000);
+            int N2 = (N / 100 % 10);
+            int N3 = (N / 10 % 10);
+            int N4 = (N % 10);
+            int S = (N1 + N2) - (N3 + N4);
+            if (S == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else {
+                Console.WriteLine(S);
+        }
+        }
+    }
+}
---
+++ 5ex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex 1-5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите часы, минуты, секунды первого момента времени");
+            int H1 = Convert.ToInt32(Console.ReadLine());
+            int M1 = Convert.ToInt32(Console.ReadLine());
+            int S1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите часы, минуты, секунды второго момента времени");
+            int H2 = Convert.ToInt32(Console.ReadLine());
+            int M2 = Convert.ToInt32(Console.ReadLine());
+            int S2 = Convert.ToInt32(Console.ReadLine());
+            int R = (H1 - H2) + ((M1 - M2) * 60) + ((S1 - S2) * 3600)
+            Console.WriteLine(R)
+        }
+    }
+}
